7b9f370b594433bb31198126d218e51c271b6732
Christian Mehlmauer <105281+firefart@users.noreply.github.com>
2024-12-04 11:12:54 +0100

    fix module

--- ifarch.py
+++ ifarch.py
@@ -1,6 +1,12 @@
 import platform
+import os
+import glob
+from typing import List
 import dotbot
+from dotbot import Plugin
 from dotbot.dispatcher import Dispatcher
+from dotbot.plugins import Clean, Create, Link, Shell
+from dotbot.util import module
 
 class IfArch(dotbot.Plugin):
     _archs = [
@@ -11,27 +17,41 @@ class IfArch(dotbot.Plugin):
     ]
 
     def __init__(self, context):
-        super(IfArch, self).__init__(context)
-        self._directives = ['if'+a for a in self._archs]
+        super().__init__(context)
+        self._directives = [f"if{a}" for a in self._archs]
 
     def can_handle(self, directive):
         return directive in self._directives
 
     def handle(self, directive, data):
         if directive not in self._directives:
-            raise ValueError('Cannot handle this directive %s' % directive)
+            raise ValueError(f"Cannot handle this directive {directive}")
 
         arch = platform.machine()
         if directive == 'if'+arch:
-            self._log.debug('Matched arch %s' % arch)
+            self._log.debug(f"Matched arch {arch}")
             return self._run_internal(data)
-        else:
-            return True
+        return True
+
+    def _load_plugins(self) -> List[Plugin]:
+        plugin_paths = self._context.options().plugins
+        plugins = []
+        for directory in self._context.options().plugin_dirs:
+            for path in glob.glob(os.path.join(directory, "*.py")):
+                plugin_paths.append(path)
+        for path in plugin_paths:
+            abspath = os.path.abspath(path)
+            plugins.extend(module.load(abspath))
+        if not self._context.options().disable_built_in_plugins:
+            plugins.extend([Clean, Create, Link, Shell])
+        return plugins
 
     def _run_internal(self, data):
-        dispatcher = Dispatcher(self._context.base_directory(),
-                                only=self._context.options().only,
-                                skip=self._context.options().skip,
-                                exit_on_failure=self._context.options().exit_on_failure,
-                                options=self._context.options())
+        dispatcher = Dispatcher(
+            self._context.base_directory(),
+            only=self._context.options().only,
+            skip=self._context.options().skip,
+            options=self._context.options(),
+            plugins=self._load_plugins(),
+        )
         return dispatcher.dispatch(data)
